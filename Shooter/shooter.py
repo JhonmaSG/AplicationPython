7cd68d2ca41f5a91f4e8eee4f70330d64a9fbe36
JhonmaSG <jmserranog@udistrital.edu.co>
2024-01-18 17:39:16 -0500

    added event meteor and player

--- Shooter/shooter.py
+++ Shooter/shooter.py
@@ -45,6 +45,43 @@ max_score = 20
 score = 0
 
 
+#Lista de Sprites, deteccion
+all_sprite_list = pygame.sprite.Group()
+meteor_list = pygame.sprite.Group()
+laser_list = pygame.sprite.Group()
+
+# # meteor
+for i in range(max_score):
+    meteor = Meteor()
+    meteor.rect.x = random.randrange(880)
+    meteor.rect.y = random.randrange(450)
+
+    # Agregar a List_sprite
+    meteor_list.add(meteor)
+    all_sprite_list.add(meteor)
+#Creacion instancia
+player = Player()
+all_sprite_list.add(player)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        # pos del laser
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            laser = Laser()
+            # pos de la nave + 45 px : center
+            laser.rect.x = player.rect.x + 45
+            laser.rect.y = player.rect.y - 40
+
+            all_sprite_list.add(laser)
+            laser_list.add(laser)
+            
+    # Orden Estricto: update, fill and sprite
+    all_sprite_list.update()
+
+    
+
 
     screen.fill(WHITE)
     all_sprite_list.draw(screen)
