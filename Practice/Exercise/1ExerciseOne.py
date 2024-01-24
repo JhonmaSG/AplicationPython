e9ecac7cebb8a2c1c8908f76639511abfec2eeed
JhonmaSG <jmserranog@udistrital.edu.co>
2024-01-24 14:30:44 -0500

    Complete Excercise 1 and 2 python

--- Practice/Exercise/1ExerciseOne.py
+++ Practice/Exercise/1ExerciseOne.py
@@ -1,3 +1,16 @@
 #1. Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
 #(Es cierto que python tiene una función max() sin usar funciones reconstruidas
 
+def num_max(num1, num2):
+    if num1 == num2:
+        return "Son iguales"
+    elif num1 > num2:
+        return num1
+    else:
+        return num2
+
+numero1 = 9
+numero2 = 5
+print("Numero 1: ",numero1)
+print("Numero 2: ",numero2)
+print("Resultado:",num_max(numero1, numero2))
--- Practice/Exercise/2ExerciseTwo.py
+++ Practice/Exercise/2ExerciseTwo.py
@@ -1,1 +1,18 @@
 # 2. Definir una función max_de_tres(), que tome tres números como argumentos y devuelva el mayor de ellos.
+
+def max_de_tres(num1, num2, num3):
+    if num1 == num2 == num3:
+        return "Son iguales"
+    elif num1 > num2 and num1 > num3:
+        return num1
+    elif num2 > num1 and num2 > num3:
+        return num2
+    else:
+        return num3
+num1 = 1
+num2 = 5
+num3 = 9
+print("Num1:",num1)
+print("Num2:",num2)
+print("Num3:",num3)
+print("El # Mayor es:",max_de_tres(num1, num2, num3))
